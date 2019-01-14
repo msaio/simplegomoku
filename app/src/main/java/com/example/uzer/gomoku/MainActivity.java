9c2f370f42aab096f2164611ac15e17bf9264999
msaio <msaiozero97@gmail.com>
2019-01-14 16:16:09 +0700

    finish

--- app/src/main/java/com/example/uzer/gomoku/MainActivity.java
+++ app/src/main/java/com/example/uzer/gomoku/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.uzer.gomoku;
 
 import android.annotation.SuppressLint;
+import android.app.AlarmManager;
 import android.app.Dialog;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.content.res.Resources;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
@@ -51,7 +55,7 @@ public class MainActivity extends AppCompatActivity {
     private int turn;
     private boolean isClicked;
     static int playWith = 1; // 0 is pve, 1 is pvp
-    private Boolean switchStateOption = false;
+//    private Boolean switchStateOption = false;
 
     CheckBox pvp;
     CheckBox pve;
@@ -69,6 +73,18 @@ public class MainActivity extends AppCompatActivity {
         setListen();
         loadResources();
         designBoardGame();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.restart,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        doRestart(this);
+        return super.onOptionsItemSelected(item);
     }
 
     private void ChangeOptionDialog() {
@@ -120,6 +136,7 @@ public class MainActivity extends AppCompatActivity {
         btnNewGame.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                btnNewGame.setText("New Game");
                 init_game();
 
                 if(playWith == 0){
@@ -506,5 +523,43 @@ public class MainActivity extends AppCompatActivity {
         return 0;
     }
 
+    public static void doRestart(Context c) {
+        try {
+            //check if the context is given
+            if (c != null) {
+                //fetch the packagemanager so we can get the default launch activity
+                // (you can replace this intent with any other activity if you want
+                PackageManager pm = c.getPackageManager();
+                //check if we got the PackageManager
+                if (pm != null) {
+                    //create the intent with the default start activity for your application
+                    Intent mStartActivity = pm.getLaunchIntentForPackage(
+                            c.getPackageName()
+                    );
+                    if (mStartActivity != null) {
+                        mStartActivity.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        //create a pending intent so the application is restarted after System.exit(0) was called.
+                        // We use an AlarmManager to call this intent in 100ms
+                        int mPendingIntentId = 223344;
+                        PendingIntent mPendingIntent = PendingIntent
+                                .getActivity(c, mPendingIntentId, mStartActivity,
+                                        PendingIntent.FLAG_CANCEL_CURRENT);
+                        AlarmManager mgr = (AlarmManager) c.getSystemService(Context.ALARM_SERVICE);
+                        mgr.set(AlarmManager.RTC, System.currentTimeMillis() + 100, mPendingIntent);
+                        //kill the application
+                        System.exit(0);
+                    } else {
+                        Log.e("test", "Was not able to restart application, mStartActivity null");
+                    }
+                } else {
+                    Log.e("test", "Was not able to restart application, PM null");
+                }
+            } else {
+                Log.e("test", "Was not able to restart application, Context null");
+            }
+        } catch (Exception ex) {
+            Log.e("test", "Was not able to restart application");
+        }
+    }
 
 }
